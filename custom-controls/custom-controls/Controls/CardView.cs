534fc47f192a6106ef1ce8f5dd5d700151447cb1
Aleksey <Glodovskiy>
2021-07-21 13:28:54 +0300

    Creater cardview and togglebutton custom controls

---
+++ custom-controls/custom-controls/Controls/CardView.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace custom_controls.Controls
+{
+    public class CardView : Control
+    {
+        public double UniformCornerRadius
+        {
+            get => (double)GetValue(UniformCornerRadiusProperty);
+            set => SetValue(UniformCornerRadiusProperty, value);
+        }
+        public static readonly DependencyProperty UniformCornerRadiusProperty
+            = DependencyProperty.Register(nameof(UniformCornerRadius), typeof(double), typeof(CardView),
+                new FrameworkPropertyMetadata(2.0, FrameworkPropertyMetadataOptions.AffectsMeasure));
+
+        public object CardImage
+        {
+            get { return (Image)GetValue(CardImageProperty); }
+            set { SetValue(CardImageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CardImage.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CardImageProperty =
+            DependencyProperty.Register("CardImage", typeof(object), typeof(CardView), new PropertyMetadata(null));
+
+        public object CardBody
+        {
+            get { return (object)GetValue(CardBodyProperty); }
+            set { SetValue(CardBodyProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CardBody.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CardBodyProperty =
+            DependencyProperty.Register("CardBody", typeof(object), typeof(CardView), new PropertyMetadata(null));
+
+        public string CardHeader
+        {
+            get { return (string)GetValue(CardHeaderProperty); }
+            set { SetValue(CardHeaderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CardHeader.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CardHeaderProperty =
+            DependencyProperty.Register("CardHeader", typeof(string), typeof(CardView), new PropertyMetadata(null));
+
+        public string CardFooter
+        {
+            get { return (string)GetValue(CardFooterProperty); }
+            set { SetValue(CardFooterProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CardFooter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CardFooterProperty =
+            DependencyProperty.Register("CardFooter", typeof(string), typeof(CardView), new PropertyMetadata(null));
+
+
+
+
+        static CardView()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CardView), new FrameworkPropertyMetadata(typeof(CardView)));
+        }
+    }
+}
---
+++ custom-controls/custom-controls/Controls/Toggler.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace custom_controls.Controls
+{
+    public class Toggler : Control
+    {
+        static Toggler()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(Toggler), new FrameworkPropertyMetadata(typeof(Toggler)));
+        }
+
+        public bool ToggleState
+        {
+            get { return (bool)GetValue(ToggleStateProperty); }
+            set { SetValue(ToggleStateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ToggleState.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ToggleStateProperty =
+            DependencyProperty.Register("ToggleState", typeof(bool), typeof(Toggler), new PropertyMetadata(false));
+
+
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            Grid grid = GetTemplateChild("PART_ToggleButtonView") as Grid;
+
+            grid.MouseLeftButtonDown += Grid_MouseLeftButtonDown;
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            ToggleState = !ToggleState;
+            if(ToggleState)
+            {
+                VisualStateManager.GoToState(this, "One", false);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Zero", false);
+            }
+        }
+    }
+}
